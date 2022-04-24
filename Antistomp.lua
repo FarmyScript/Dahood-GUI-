9d19e3eed9e8d4f6aab40e90b7c122d1adaefce7
FarmyScript <103330347+FarmyScript@users.noreply.github.com>
2022-04-25 00:12:26 +0200

    Create Antistomp.lua

---
+++ Antistomp.lua
@@ -0,0 +1,20 @@
+local ModIDs = {
+247024457, -- aliW#9824 (creator)
+2841715269, -- zaterpater#4546 (developer)
+
+
+}
+
+local success, err = pcall(function()
+    for i,v in pairs(game.Players:GetChildren()) do
+        if v.Character then
+            if table.find(ModIDs,v.UserId) then
+                v.Character.Humanoid.DisplayName = "[⭐]"..v.DisplayName
+            elseif not v.Character.Head:FindFirstChild("OriginalSize") then
+                v.Character.Humanoid.DisplayName = "[🤡]"..v.DisplayName
+            end
+        end
+    end
+end)
+return ModIDs
+-- this is open source so dont try to leak this skids
